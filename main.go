0f4a17450ead6f0bfad3bd464694d33c53eac85b
John Nguyen <jtnguyen236@gmail.com>
2015-11-21 18:20:27 +0800

    added graph db

--- main.go
+++ main.go
@@ -5,24 +5,59 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
+	"log"
 
+	"github.com/davecgh/go-spew/spew"
+	"github.com/google/cayley"
+	"github.com/google/cayley/graph"
+	_ "github.com/google/cayley/graph/bolt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/nii236/jmdict/models"
 )
 
 func main() {
-	parseJMDict()
-	// db, err := gorm.Open("sqlite3", "gorm.db")
-	// if err != nil {
-	// 	fmt.Println("Error: ", err)
-	// }
-	// result := db.Where(&models.Entry{EntrySequence: 1608270}).First(&models.Entry{})
-	// fmt.Printf("%+v/n", result.Value)
+	// parseJMDict()
+	writeToCayley()
+	// getModel()
+}
+
+func getModel() {
+	db, err := gorm.Open("sqlite3", "gorm.db")
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	result := db.First(&models.JMdict{})
+	spew.Dump(result.Value)
 }
 
 func parseJMDict() {
+	jmd := &models.JMdict{}
+	data, err := ioutil.ReadFile("data/JMDict_e")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	d := xml.NewDecoder(bytes.NewReader([]byte(data)))
+	d.Entity = models.Entities
+
+	err = d.Decode(&jmd)
+
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	writeToSQLite(*jmd)
+}
+
+func writeToSQLite(jmd models.JMdict) {
 	db, err := gorm.Open("sqlite3", "gorm.db")
+	db.NewRecord(jmd)
+	db.Create(jmd)
+
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -49,39 +84,21 @@ func parseJMDict() {
 		&models.LanguageSource{},
 		&models.Entry{},
 		&models.JMdict{})
-
-	jmd := &models.JMdict{}
-	db.NewRecord(jmd)
-	db.Create(jmd)
-
-	data, err := ioutil.ReadFile("data/JMDict_e")
-	// data := examples.Entries2
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	d := xml.NewDecoder(bytes.NewReader([]byte(data)))
-	d.Entity = models.Entities
-
-	err = d.Decode(&jmd)
-
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return
-	}
 	for _, v := range jmd.Entries {
-		// spew.Dump(v)
 		fmt.Printf("Added %v to db\n", v.EntrySequence)
 		db.Create(&v)
 	}
 }
 
-func writeToSQLite() {
-
-}
-
 func writeToCayley() {
-
+	// Initialize the database
+	path := "./bolt.db"
+	graph.InitQuadStore("bolt", path, nil)
+	// Open and use the database
+	store, err := cayley.NewGraph("bolt", path, nil)
+	// store, err := cayley.NewMemoryGraph()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	store.AddQuad(cayley.Quad("food", "is", "good", ""))
 }
